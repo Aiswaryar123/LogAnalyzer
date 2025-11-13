225b08a9e88d1e8348aaa34e73209631b9ab0929
Aiswarya r <aiswaryarathee123@gmail.com>
2025-11-13 14:25:46 +0530

    connects program to postgresql and insert all parsed log entries to a database

---
+++ cmd/database_insertion/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"miniproject/database"
+	"miniproject/segment"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	logPath := flag.String("path", "/home/user/miniproject/logs", "Path to the log directory")
+	flag.Parse()
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, os.Getenv("database_url"))
+	if err != nil {
+		slog.Error("Unable to connect to database", "error", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+	logStore, err := segment.ParseLogSegments(*logPath)
+	if err != nil {
+		slog.Error("Failed to parse logs", "error", err)
+
+	}
+
+	err = database.InsertLogs(ctx, conn, logStore)
+	if err != nil {
+		slog.Error("Failed to insert logs", "error", err)
+	} else {
+		slog.Info("All logs inserted successfully")
+	}
+}
---
+++ database/insert.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"context"
+	"log/slog"
+	models "miniproject/model"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func InsertLogs(ctx context.Context, conn *pgx.Conn, store models.LogStore) error {
+	for _, segment := range store.Segments {
+		for _, entry := range segment.LogEntries {
+			_, err := conn.Exec(ctx, `
+				INSERT INTO log_entry (time, level, host, component, requestid, msg)
+				VALUES (
+					$1,
+					(SELECT id FROM log_level WHERE level = $2),
+					(SELECT id FROM log_host WHERE host = $3),
+					(SELECT id FROM log_component WHERE component = $4),
+					$5,
+					$6
+				)`,
+				entry.Time,
+				entry.Level,
+				entry.Host,
+				entry.Component,
+				entry.Requestid,
+				entry.Message,
+			)
+			if err != nil {
+				slog.Warn("Failed to insert log entry", "error", err, "entry", entry.Raw)
+				continue
+			}
+		}
+	}
+	return nil
+}
